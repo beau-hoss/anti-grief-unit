3a5c893d00b4c97ea48340d7feba89315ddfa251
beau-hoss <shapeswerke@gmail.com>
2026-03-10 04:09:35 -0400

    Update signout.js

--- api/signout.js
+++ api/signout.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   if (!badge) return res.status(400).json({ error: 'Badge number required' });
 
   try {
-    const webhookUrl = 'https://discord.com/api/webhooks/1475657960926740574/oQwjQEhJz53vv4iHy81Cpc1tWdIrWsHbAEwLXi2onHt4Je6Z8w4jFhhDcWb1QcA33a8q';
+    const webhookUrl = 'https://discord.com/api/webhooks/1480838943690854534/v1yOHbJOqwYMle_HWhrZzGKBjOVvCAO5cQ0IpC-QDuqTpvcEX_8PpoETQmBmnc6n4-wB';
     const message = {
       content: `🔴 Officer **#${badge}** has signed OFF DUTY.`,
     };
